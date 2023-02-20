e381985dcbdcacf51407d95ec58020633e9c582c
Alex-Lekov <61644712+Alex-Lekov@users.noreply.github.com>
2023-02-20 18:03:03 +0100

    refactory

--- app/load_history.py
+++ app/load_history.py
@@ -9,8 +9,9 @@ from decimal import Decimal
 from cryptofeed.exchanges import BinanceFutures
 import clickhouse_driver
 
-####### Logger #########################################################
+####### Logger Configuration #########################################################
 
+# Remove the default sink and add a new sink to stderr with a custom format and log level
 logger.remove()
 logger.add(
     sys.stderr,
@@ -19,22 +20,26 @@ logger.add(
     level=10,
 )
 
+# Add a file sink with DEBUG level and compression
 logger.add("log.log", rotation="1 MB", level="DEBUG", compression="zip")
 
-####### LOAD CONFIG #########################################################
+####### Load Config #########################################################
+
 with open("config.yaml", 'r') as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
 
-START_DATE = config['START_DATE']
-LOAD_HISTORY = config['LOAD_HISTORY']
+START_DATE: str = config['START_DATE']
+LOAD_HISTORY: bool = config['LOAD_HISTORY']
 
-####### Func #########################################################
-def candle_save(candle, receipt_timestamp) -> None:
-    """function that stores candle data into ClickHouse.
+
+####### Functions #########################################################
+
+def candle_save(candle, receipt_timestamp: datetime.datetime) -> None:
+    """Inserts the given candle data into ClickHouse.
 
     Args:
-        candle: The candle data.
-        receipt_timestamp: The receipt timestamp.
+        candle: A dictionary containing candle data.
+        receipt_timestamp: A datetime object representing the receipt timestamp.
 
     """
     exchange = candle.exchange
@@ -62,7 +67,13 @@ def candle_save(candle, receipt_timestamp) -> None:
         ch.execute(query, {'exchange': exchange, 'symbol': symbol, 'start': start, 'stop': stop, 'interval': interval, 'trades': trades, 'open': open_price, 'close': close_price, 'high': high_price, 'low': low_price, 'volume': volume, 'timestamp': timestamp, 'receipt_timestamp': receipt_timestamp})
 
 
-def get_symbols_last_date():
+def get_symbols_last_date() -> dict:
+    """Returns a dictionary containing the last date for each symbol in the ClickHouse database.
+
+    Returns:
+        A dictionary containing symbol names as keys and the corresponding last date as values.
+
+    """
     with clickhouse_driver.Client(host='clickhouse', port=9000) as ch:
         result = ch.execute('SELECT symbol, MIN(stop) as min_date FROM binance_data.candles GROUP BY symbol')
     
@@ -73,14 +84,17 @@ def get_symbols_last_date():
 
 
 ####### Main #########################################################
+
 if __name__ == '__main__':
     if LOAD_HISTORY:
         logger.info(f'Delay start by 120sec so that BD are ready')
         time.sleep(120)
         logger.info(f'START HISTORY LOAD')
 
+        # Get the last stop date for each symbol
         symbol_last_data_dict = get_symbols_last_date()
 
+        # Loop through each symbol and load its history
         for symbol, last_date in tqdm(symbol_last_data_dict.items()):
             logger.info(f'Load history: {symbol}')
             for data in tqdm(BinanceFutures().candles_sync(symbol, start=START_DATE, end=last_date)):
